fd7515ab18aae3ef0b9411c93d8c801b368b88ae
John Dowson <john.dowson@sse.com>
2021-08-20 10:43:47 +0100

    Remove unused imports

--- nornir_pyez/plugins/tasks/pyez_commit.py
+++ nornir_pyez/plugins/tasks/pyez_commit.py
@@ -1,13 +1,12 @@
-import copy
-from typing import Any, Dict, List, Optional
 from jnpr.junos.utils.config import Config
 from nornir.core.task import Result, Task
 
 from nornir_pyez.plugins.connections import CONNECTION_NAME
 
 
-def pyez_commit(task: Task,
-                ) -> Result:
+def pyez_commit(
+    task: Task,
+) -> Result:
     device = task.host.get_connection(CONNECTION_NAME, task.nornir.config)
     device.timeout = 300
     config = Config(device)
